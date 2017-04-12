84863b86eedd940ecba44fe881e6b938da1dadaf
unbug <tidelgl@gmail.com>
2017-04-12 15:20:00 +0800

    revemod old docs

--- gulp-tasks/clean.js
+++ gulp-tasks/clean.js
@@ -2,10 +2,14 @@ var gulp = require('gulp-help')(require('gulp'));
 var del = require('del');
 var runSequence = require('run-sequence');
 
-gulp.task('clean:dist', 'Cleans dist files', function (cb) {
+gulp.task('clean:dist', 'Cleans dist files', function () {
   return del(['./dist/**'], {force: true});
 });
 
+gulp.task('clean:docs', 'Cleans docs files', function () {
+  return del(['./docs/html/**', './docs/*.md'], {force: true});
+});
+
 gulp.task('clean', 'Cleans files', function (cb) {
-  runSequence('clean:dist', cb);
+  runSequence('clean:dist', 'clean:docs', cb);
 });
